a8515c3f2d35c82739f05707c054ab347a211b2c
Its-Nishant-01 <n1shanntjha@gmail.com>
2022-07-16 03:20:50 +0530

    2.14

--- gpt3.py
+++ gpt3.py
@@ -10,7 +10,7 @@ prompt_text = st.text_input(label="Ask something...")
 response = openai.Completion.create(
     engine="davinci-instruct-beta-v3",
     max_tokens=500,
-    prompt="Expand the prompt text in to a detailed professional and clever explanation.\n\n {}".format(prompt_text),
+    prompt="Expand the prompt text in to a detailed professional and clever explanation.".format(prompt_text),
     temperature=0.7,
     top_p=1,
     frequency_penalty=0,
