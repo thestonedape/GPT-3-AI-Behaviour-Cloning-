21c62d21b0126110527bdfcb3c82bae63f18300d
Its-Nishant-01 <n1shanntjha@gmail.com>
2022-07-16 02:59:59 +0530

    1.6

--- gpt3.py
+++ gpt3.py
@@ -3,12 +3,21 @@ import streamlit as st
 openai.api_key = st.secrets["SECRET_KEY"]
 
 
-st.title('GPT-3')
+st.title('The Stoned Ape')
 
-st.text('This GPT-3 model is the Davinci-engine')
-prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
-response = openai.Completion.create(engine="davinci", prompt=prompt_text, max_tokens=5)
-st.text('Remaining phrase:')
+st.text('This is an experiment of prompt designing by using GPT-3, a neural network trained and hosted by OpenAI.')
+prompt_text = st.text_input(label="Ask something...", value="Add here few words")
+response = openai.Completion.create(
+    engine="davinci-instruct-beta-v3",
+    prompt=prompt_text,
+    max_tokens=500,
+    prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(prompt_text),
+    temperature=0.7,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+st.text('Output:')
 st.text(response["choices"][0]["text"])
 
 
