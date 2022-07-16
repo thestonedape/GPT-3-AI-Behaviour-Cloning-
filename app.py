3a4d59eecbcffa3a6a322e7cf4531a99309b8098
Its-Nishant-01 <n1shanntjha@gmail.com>
2022-07-16 07:22:32 +0530

    2.2222

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ openai.api_key = st.secrets["SECRET_KEY"]
 st.title('The Stoned Ape')
 st.caption('Backed by Inside Labs')
 
-st.subheader('This is an experiment of prompt designing by using GPT-3(a transformer model), a neural network trained and hosted by OpenAI.')
-st.markdown('Tips: Try to ask specific detaied questions, like "Who are you?')
+st.markdown('This is an experiment of prompt designing by using GPT-3(a transformer model), a neural network trained and hosted by OpenAI.')
+st.caption('Tips: Try to ask specific detaied questions, like "Who are you?')
 prompt_text = st.text_input(label="Input" , value="Ask me anything!")
 response = openai.Completion.create(
     engine="davinci-instruct-beta-v3",
