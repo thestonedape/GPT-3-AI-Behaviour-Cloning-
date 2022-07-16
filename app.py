1ee779defbff4afd85a18b1ace44acce980c51c6
Its-Nishant-01 <n1shanntjha@gmail.com>
2022-07-16 06:28:19 +0530

    5.635

--- app.py
+++ app.py
@@ -17,21 +17,20 @@ openai.api_key = st.secrets["SECRET_KEY"]
 
 st.title('The Stoned Ape')
 
-st.text('This is an experiment of prompt designing by using GPT-3,a neural network trained and hosted by OpenAI.')
-prompt_text = st.text_input(label="Input")
+st.markdown('This is an experiment of prompt designing by using GPT-3(a transformer model), a neural network trained and hosted by OpenAI.')
+st.markdown('tips: Try to ask specific detaied questions, like "What is the meaning of "the" in "the meaning of life"?')
+prompt_text = st.text_input(label="Input" , value="Ask me anything!")
 response = openai.Completion.create(
     engine="davinci-instruct-beta-v3",
     max_tokens=500,
-    prompt="Expand the prompt text in to a detailed professional and creative explanation.\n\n {}".format(prompt_text),
-    temperature=0.7,
+    prompt="Expand the prompt text in to a detailed philosophical and creative explanation.\n\n {}".format(prompt_text),
+    temperature=0.8,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0
     )
 st.text('Output:')
 #on click, the response is displayed
-
-if st.button('Generate'):
-    st.markdown(response["choices"][0]["text"])
+st.markdown_output(response["choices"][0]["text"])
 
 
