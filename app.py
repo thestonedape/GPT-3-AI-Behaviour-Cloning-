2ec31b10660f44136600094ed3edef41e9df584c
Its-Nishant-01 <n1shanntjha@gmail.com>
2022-07-16 06:40:48 +0530

    1.023

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ streamlit_style = """
 			}
 			</style>
 			"""
-st.markdown(streamlit_style)
+st.markdown(streamlit_style, unsafe_allow_html=1)
 
 
 openai.api_key = st.secrets["SECRET_KEY"]
 
 st.title('The Stoned Ape')
 
-st.markdown('This is an experiment of prompt designing by using GPT-3(a transformer model), a neural network trained and hosted by OpenAI.', unsafe_allow_html=False)
-st.markdown('tips: Try to ask specific detaied questions, like "What is the meaning of "the" in "the meaning of life"?', unsafe_allow_html=False)
+st.markdown('This is an experiment of prompt designing by using GPT-3(a transformer model), a neural network trained and hosted by OpenAI.', unsafe_allow_html=0)
+st.markdown('tips: Try to ask specific detaied questions, like "What is the meaning of "the" in "the meaning of life"?', unsafe_allow_html=0)
 prompt_text = st.text_input(label="Input" , value="Ask me anything!")
 response = openai.Completion.create(
     engine="davinci-instruct-beta-v3",
