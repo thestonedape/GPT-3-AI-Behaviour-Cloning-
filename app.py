5b5ed9774ec57e7ed75c0055a8f5130c2fbbfff5
Its-Nishant-01 <n1shanntjha@gmail.com>
2022-07-18 02:05:25 +0530

    1.023.25

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.caption('Still in development.')
 st.caption('Backed by [Inside Labs](https://insidelibrary.weebly.com/)')
 
 st.markdown('This is an experiment of prompt designing by using GPT-3(A Transformer based model), a neural network trained and hosted by OpenAI.')
-st.caption('Tips: Try to ask specific detaied questions, like "Who are you?')
+st.caption('Tips: Try to ask specific detaied questions, like "Who are you?"')
 prompt_text = st.text_input(label="Input" , value="Ask me anything!")
 response = openai.Completion.create(
     engine="davinci-instruct-beta-v3",
